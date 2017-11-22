d103fe97eac6dac4b0c331d45726cffa3a972834
HFrajacomo <henrique.frajacomo@gmail.com>
2017-11-22 02:46:31 -0200

    Added headers and collisions

--- Test Scripts/Movement.asm
+++ Test Scripts/Movement.asm
@@ -2,25 +2,30 @@
 
 Include ..\Irvine32.inc
 Include ..\win32.inc
+nColumns = 64
+nRows = 16
 
 .data
-	matriz BYTE 32*16 dup(00h)
+	matriz BYTE nColumns*nRows dup(00h)
 	
-	screen_buffer BYTE 32*16 dup(0)
+	screen_buffer BYTE nColumns*nRows dup(0)
 	carry BYTE ?
-	player_x DWORD 10  ; 6-30  // 0 - 31
-	player_y DWORD 14  ; 0-31
+	player_x DWORD 6  ; 6-62  // 0 - 63
+	player_y DWORD 0  ; 0-31
 	foodwaitingtime DWORD 30
+	score DWORD 0
+
+	tScore BYTE "Score: ", 0
 	
 	
 	
-	                               ; ><(((º>     
+	                               ; ><(((ยบ>     
 								   
 								   ; Ascii Characters
 								   ; > = 62
 								   ; < = 60
 								   ; ( = 40
-								   ; º = 167
+								   ; ยบ = 167
 								   ; * = 42
 								   ; + = 43
 
@@ -82,14 +87,22 @@ L1:
 	mov matriz[ecx], al
 	
 	; Clears back row
-	mov matriz[31], 0 
-	mov matriz[63], 0
-	mov matriz[95], 0
+	mov matriz[63], 0 
 	mov matriz[127], 0
-	mov matriz[159], 0
 	mov matriz[191], 0
-	mov matriz[223], 0
 	mov matriz[255], 0
+	mov matriz[319], 0
+	mov matriz[383], 0
+	mov matriz[447], 0
+	mov matriz[511], 0
+	mov matriz[575], 0
+	mov matriz[639], 0
+	mov matriz[703], 0
+	mov matriz[767], 0
+	mov matriz[831], 0
+	mov matriz[895], 0
+	mov matriz[959], 0
+	mov matriz[1023], 0
 	
 	pop edx
 	pop ebx
@@ -106,13 +119,14 @@ DrawScreen PROC
 	push edx
 	push ebx
 	
-	mov edx, 0    ; Posiciona o cursor no começo do terminal
-	call GOTOXY
+	; Posiciona o cursor após o header
+	call GameCursor
+	call SetColor_Blue
 	
-	mov edx, 16  ; Screen y Size
+	mov edx, nRows
 	mov ebx, 0
 L3:
-	mov ecx, 32
+	mov ecx, nColumns
 L2:
 	mov al, screen_buffer[ebx]
 	call WriteChar
@@ -147,7 +161,7 @@ L4:
 	inc eax
 	loop L4             ; ----------
 	
-	mov eax, 32 ; Find Player position
+	mov eax, nColumns ; Find Player position
 	mul player_y
 	add eax, player_x
 	
@@ -207,21 +221,28 @@ Move PROC
 	je right
 	cmp al, 73h
 	je down
+	cmp al, "q"
+	jz q
 	jmp sair
+	
+q:
+	call SetColor_Default
+	call Clrscr
+	exit
 	
 left:
 	mov ebx, player_x
 	cmp ebx, 6
 	jbe sair
-	dec ebx
+	sub ebx, 1
 	mov player_x, ebx
 	jmp sair
 
 right:
 	mov ebx, player_x
-	cmp ebx, 31
+	cmp ebx, nColumns-1
 	jae sair
-	inc ebx
+	add ebx, 1
 	mov player_x, ebx
 	jmp sair
 
@@ -229,15 +250,15 @@ up:
 	mov ebx, player_y
 	cmp ebx, 0
 	jbe sair
-	dec ebx
+	sub ebx, 1
 	mov player_y, ebx
 	jmp sair
 
 down:
 	mov ebx, player_y
-	cmp ebx, 15
+	cmp ebx, nRows-1
 	jae sair
-	inc ebx
+	add ebx, 1
 	mov player_y, ebx
 	
 sair:
@@ -259,16 +280,16 @@ Clear PROC
 	mov ecx, LENGTHOF screen_buffer
 	mov al, " "
 	mov ebx, 0
-	mov edx, 0
+	mov dx, 0200h 
 L5:
 	call GOTOXY
 	call WriteChar
 	inc dl
-	cmp dl, 16
+	cmp dl, nRows
 	jne L5
 	add dx, 0100h
 	mov dl, 0
-	cmp dx, 2000h
+	cmp dx, 4200h
 	je L6
 L6:
 	
@@ -291,6 +312,18 @@ FixCursor PROC
 	ret
 FixCursor ENDP
 
+; Sets the cursor to the first pixel of the game screen
+; NO PARAMETERS
+GameCursor PROC
+	push edx
+
+	mov edx, 0200h
+	call GOTOXY
+	
+	pop edx
+	ret
+GameCursor ENDP
+
 ; Generates food into the screen matrix
 ; Arguments: foodwaitingtime
 CreateFood PROC
@@ -304,14 +337,14 @@ CreateFood PROC
 	ja L7	  ; Ends function
 	
 	; Else, allocates a food to the screen matrix
-	mov eax, 16  
+	mov eax, nRows  
 	call RandomRange
 	mov ah, 0
-	mov bl, 32
+	mov bl, nColumns
 	mul bl
 	mov ebx, eax
 	mov al, "+"
-	mov matriz[ebx+31], al
+	mov matriz[ebx+63], al  ; nColumns-1
 	mov eax, 50  ; Next waiting time
 	call RandomRange
 	mov ecx, eax
@@ -333,21 +366,163 @@ L8:
 	ret
 CreateFood ENDP
 
+; Sets foreground to red and background to black
+; NO PARAMETERS
+SetColor_Red PROC
+	push eax
+
+	mov eax, red+(black*16)
+	call SetTextColor
+	
+	pop eax
+	ret
+SetColor_Red ENDP
+
+
+; Sets foreground to white and background to lightblue
+; NO PARAMETERS
+SetColor_Blue PROC
+	push eax
+
+	mov eax, White+(lightblue*16)
+	call SetTextColor
+	
+	pop eax
+	ret
+SetColor_Blue ENDP
+
+; Sets terminal colors back to default
+; NO PARAMETERS
+SetColor_Default PROC
+	push eax
+
+	mov eax, white+(black*16)
+	call SetTextColor
+	
+	pop eax
+	ret
+SetColor_Default ENDP
+
+; Draws score header above game screen
+; NO PARAMETERS
+DrawHeader PROC
+	push edx
+	push eax
+	push ecx
+	
+	; Draws Scoreboard
+	call FixCursor
+	call SetColor_Red
+	mov edx, OFFSET tScore
+	call WriteString
+	mov eax, score
+	call WriteDec
+	
+	; Draws Upper Border
+	mov al, 3Dh 
+	mov edx, 0100h
+	call GOTOXY
+	mov ecx, nColumns
+	
+L9: 
+	call WriteChar
+	loop L9 
+	
+	;Draws Lower Border
+	mov edx, 0
+	mov dh, nRows
+	add dh, 2
+	call GOTOXY
+	mov ecx, nColumns
+
+L10: 
+	call WriteChar
+	loop L10 	
+	
+	pop ecx
+	pop eax
+	pop edx
+	ret
+DrawHeader ENDP
+
+Collision PROC
+	push eax
+	push ebx
+
+	call Get_Index  ; Gets overall player position
+	mov ebx, 0
+	
+	; Merges screen_buffer with screen_matrix to find collisions
+	cmp matriz[eax], 0
+	ja L11
+	cmp matriz[eax-1], 0
+	inc ebx
+	ja L11
+	cmp matriz[eax-2], 0
+	inc ebx
+	ja L11
+	cmp matriz[eax-3], 0
+	inc ebx
+	ja L11
+	cmp matriz[eax-4], 0
+	inc ebx
+	ja L11
+	cmp matriz[eax-5], 0
+	inc ebx
+	ja L11
+	cmp matriz[eax-6], 0
+	inc ebx
+	ja L11
+	jmp L12
+	
+L11: ; Found a collision
+	sub eax, ebx
+	cmp matriz[eax], "+"
+	je HitFood
+	jmp EnemyHit
+
+HitFood: ; Collided to food
+	mov matriz[eax], 0
+	mov eax, score
+	inc eax
+	mov score, eax
+
+EnemyHit:  ; Collided to enemy
+
+L12:  ; Didn't Collide	
+	pop ebx
+	pop eax
+	ret
+Collision ENDP
+
+; Reads player x and y information and returns the position index on the screen
+; RETURNS: eax = matriz index value
+Get_Index PROC
+	
+	mov eax, player_y
+	shl eax, 6
+	add eax, player_x
+	
+	ret
+Get_Index ENDP
+
 main PROC
 	call Clrscr
 	call Randomize
-	
+	call SetColor_Blue
 las:
 	call Clear
 	call CreateFood
 	call DrawFish
+	call DrawHeader
 	call DrawScreen
 	call FixCursor
 	call FPSRate
-	call Move
 	call Shift
+	call Collision
+	call Move
 	jmp las
-
+	
 	exit
 main ENDP
 END main
